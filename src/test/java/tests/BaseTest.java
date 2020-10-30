1aee61f0ec55ec560c8ccbbb8dafd409ab78a21e
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-30 16:25:10 +0300

    Added BaseTest

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import pages.LoginPage;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+@Listeners(TestListener.class)
+public class BaseTest {
+
+    LoginPage loginPage;
+    String email = "";
+    String password = "";
+
+    @BeforeMethod
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+        Configuration.timeout = 10000;
+        Configuration.headless = false;
+        loginPage = new LoginPage();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        getWebDriver().quit();
+    }
+}
