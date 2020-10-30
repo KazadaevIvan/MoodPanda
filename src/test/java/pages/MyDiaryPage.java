91c36173da86b43825f1fd93dc475c6be92a9473
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-30 16:26:09 +0300

    Added MyDiaryPage

---
+++ src/test/java/pages/MyDiaryPage.java
@@ -0,0 +1,47 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.testng.Assert.assertEquals;
+
+public class MyDiaryPage extends BasePage {
+    public static final String URL = "https://moodpanda.com/Feed/?Me=1";
+    public static final String DATE_CSS = ".text-muteda:first-child";
+    public static final String MOOD_RATING_CSS = ".badge.pull-right";
+    public static final String DESCRIPTION = "(//*[contains(@class,'media-content')])[1]";
+
+    @Override
+    public MyDiaryPage openPage() {
+        open(URL);
+        return this;
+    }
+
+    @Override
+    public MyDiaryPage isPageOpened() {
+        $(By.xpath(DESCRIPTION)).waitUntil(Condition.visible, 10000);
+        return this;
+    }
+
+    public MyDiaryPage validateMood(int moodRating, String description, String year, String month,
+                                    String day) {
+
+        assertEquals($(DATE_CSS).getText(), dateConstructor(year, month, day), "Date should be " + dateConstructor(year, month, day));
+        assertEquals($(MOOD_RATING_CSS).getText(), String.valueOf(moodRating), "Mood rating should be " + moodRating);
+        assertEquals($(By.xpath(DESCRIPTION)).getText(), description, "Description should be " + description);
+        return this;
+    }
+
+    public MyDiaryPage validateMood(int moodRating, String description) {
+
+        assertEquals($(MOOD_RATING_CSS).getText(), String.valueOf(moodRating), "Mood rating should be " + moodRating);
+        assertEquals($(By.xpath(DESCRIPTION)).getText(), description, "Description should be " + description);
+        return this;
+    }
+
+    public String dateConstructor(String year, String month, String day) {
+        return day + " " + month + " " + year + " · NEW";
+    }
+}
