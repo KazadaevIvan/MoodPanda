9038ddfdad2b73e40972ed2d4d3fc4fbc517834f
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-11-01 17:31:22 +0300

    Added isModalOpened method

--- src/test/java/pages/MoodUpdateModal.java
+++ src/test/java/pages/MoodUpdateModal.java
@@ -1,12 +1,20 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class MoodUpdateModal {
+public class MoodUpdateModal extends BaseModal {
 
     public static final String GO_TO_MY_DIARY_BUTTON_CSS = ".ButtonMyDiary";
+
+    @Step("Verify Mood Update modal is opened")
+    @Override
+    public BaseModal isModalOpened() {
+        $(GO_TO_MY_DIARY_BUTTON_CSS).waitUntil(Condition.visible, 10000);
+        return this;
+    }
 
     @Step("Click Go to My Diary button")
     public MyDiaryPage goToMyDiary() {
--- src/test/java/pages/RateYourHappinessModal.java
+++ src/test/java/pages/RateYourHappinessModal.java
@@ -1,12 +1,14 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.Select;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class RateYourHappinessModal {
+public class RateYourHappinessModal extends BaseModal {
 
     public static final String SLIDER_CSS = ".ui-slider-handle";
     public static final String TEXTAREA_CSS = "#TextBoxUpdateMoodTag";
@@ -18,6 +20,14 @@ public class RateYourHappinessModal {
     public static final String DAY = "//a[text()='%s']";
     public static final String UPDATE_BUTTON_CSS = ".btn.ButtonUpdate";
 
+    @Step("Verify Rate Your Happiness modal is opened")
+    @Override
+    public BaseModal isModalOpened() {
+        $(UPDATE_BUTTON_CSS).waitUntil(Condition.visible, 10000);
+        return this;
+    }
+
+    @Step("Update mood. Set mood rating '{moodRating}', date '{date}', description '{description}'")
     public MoodUpdateModal updateMood(int moodRating, String description, String date) {
 
         setMoodRating(moodRating);
@@ -27,6 +37,7 @@ public class RateYourHappinessModal {
         return new MoodUpdateModal();
     }
 
+    @Step("Update mood. Set mood rating '{moodRating}', date '{date}', time '{hours}{minutes}', description '{description}'")
     public MoodUpdateModal updateMood(int moodRating, String description, String date, String hours, String minutes) {
 
         setMoodRating(moodRating);
@@ -37,6 +48,7 @@ public class RateYourHappinessModal {
         return new MoodUpdateModal();
     }
 
+    @Step("Update mood. Set mood rating '{moodRating}', date '{date} {day} {month} {year}', time '{hours}{minutes}', description '{description}'")
     public MoodUpdateModal updateMood(int moodRating, String description, String date, String year, String month,
                                       String day, String hours, String minutes) {
         setMoodRating(moodRating);
