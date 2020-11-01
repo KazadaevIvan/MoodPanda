b21cb77d470f2150f036edacca6b2f112010a6cb
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-11-01 14:45:09 +0300

    Updated date assertion in the validateMood method. Updated dateConstructor method

--- src/test/java/pages/MyDiaryPage.java
+++ src/test/java/pages/MyDiaryPage.java
@@ -29,7 +29,8 @@ public class MyDiaryPage extends BasePage {
     public MyDiaryPage validateMood(int moodRating, String description, String year, String month,
                                     String day) {
 
-        assertEquals($(DATE_CSS).getText(), dateConstructor(year, month, day), "Date should be " + dateConstructor(year, month, day));
+        String date = $(DATE_CSS).getText();
+        assertEquals(date.substring(0, date.indexOf('·')).trim(), dateConstructor(year, month, day), "Date should be " + dateConstructor(year, month, day));
         assertEquals($(MOOD_RATING_CSS).getText(), String.valueOf(moodRating), "Mood rating should be " + moodRating);
         $(By.xpath(DESCRIPTION)).waitUntil(Condition.visible, 10000);
         assertEquals($(By.xpath(DESCRIPTION)).getText(), description, "Description should be " + description);
@@ -44,6 +45,6 @@ public class MyDiaryPage extends BasePage {
     }
 
     public String dateConstructor(String year, String month, String day) {
-        return day + " " + month + " " + year + " · NEW";
+        return day + " " + month + " " + year;
     }
 }
