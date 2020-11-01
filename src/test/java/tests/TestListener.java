b2d59480ad178fba631bf8614c5fb61816b881c3
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-11-01 17:30:16 +0300

    Optimized imports and reformatted code

--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -1,7 +1,6 @@
 package tests;
 
 import io.qameta.allure.Attachment;
-
 import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -62,12 +61,12 @@ public class TestListener implements ITestListener {
         try {
             WebDriver driver = getWebDriver();
             if (driver != null) {
-                return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+                return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
             } else {
-                return new byte[] {};
+                return new byte[]{};
             }
         } catch (NoSuchSessionException | IllegalStateException ex) {
-            return new byte[] {};
+            return new byte[]{};
         }
     }
 
