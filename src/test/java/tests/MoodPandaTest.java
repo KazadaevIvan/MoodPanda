2aed9b8a0f83a504db1f9ad654ae0dd3fdcf188e
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-11-01 13:14:47 +0300

    Added tests

--- src/test/java/tests/MoodPandaTest.java
+++ src/test/java/tests/MoodPandaTest.java
@@ -5,16 +5,71 @@ import org.testng.annotations.Test;
 public class MoodPandaTest extends BaseTest {
 
     @Test()
-    public void login() {
+    public void postWithDateOlderShouldBeCreated() {
         loginPage
                 .openPage()
                 .login(email, password)
                 .isPageOpened()
                 .clickUpdate()
-                .updateMood(8, "Description", "Older", "2019", "Dec", "26",
+                .updateMood(5, "Description", "Older", "2019", "Dec", "26",
                         "3 am", ":45")
                 .goToMyDiary()
                 .isPageOpened()
-                .validateMood(8, "Description", "2019", "Dec", "26");
+                .validateMood(5, "Description", "2019", "Dec", "26");
+    }
+
+    @Test()
+    public void postWithDateThreeDaysAgoShouldBeCreated() {
+        loginPage
+                .openPage()
+                .login(email, password)
+                .isPageOpened()
+                .clickUpdate()
+                .updateMood(1, "Description", "3 days ago",
+                        "1 am", ":30")
+                .goToMyDiary()
+                .isPageOpened()
+                .validateMood(1, "Description");
+    }
+
+    @Test()
+    public void postWithDateTwoDaysAgoShouldBeCreated() {
+        loginPage
+                .openPage()
+                .login(email, password)
+                .isPageOpened()
+                .clickUpdate()
+                .updateMood(10, "Description", "2 days ago",
+                        "9 pm", ":00")
+                .goToMyDiary()
+                .isPageOpened()
+                .validateMood(10, "Description");
+    }
+
+    @Test()
+    public void postWithDateYesterdayAgoShouldBeCreated() {
+        loginPage
+                .openPage()
+                .login(email, password)
+                .isPageOpened()
+                .clickUpdate()
+                .updateMood(9, "Description", "Yesterday",
+                        "Midday", ":15")
+                .goToMyDiary()
+                .isPageOpened()
+                .validateMood(9, "Description");
+    }
+
+    @Test()
+    public void postWithDateNowShouldBeCreated() {
+        loginPage
+                .openPage()
+                .login(email, password)
+                .isPageOpened()
+                .clickUpdate()
+                .updateMood(2, "Description", "Now")
+                .goToMyDiary()
+                .isPageOpened()
+                .validateMood(2, "Description");
     }
 }
