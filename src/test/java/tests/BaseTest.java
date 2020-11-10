d9310dcecbbe190b0471149b2ede53b9b3b4c1f9
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-11-10 17:28:33 +0300

    Updated email, password and browser values

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import pages.LoginPage;
-import utils.PropertyReader;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
@@ -13,12 +12,12 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 public class BaseTest {
 
     LoginPage loginPage;
-    String email = System.getenv().getOrDefault("email", PropertyReader.getProperty("email"));
-    String password = System.getenv().getOrDefault("password", PropertyReader.getProperty("password"));
+    String email = System.getProperty("email");
+    String password = System.getProperty("password");
 
     @BeforeMethod
     public void setUp() {
-        Configuration.browser = "chrome";
+        Configuration.browser = System.getProperty("browser");
         Configuration.startMaximized = true;
         Configuration.timeout = 10000;
         Configuration.headless = true;
